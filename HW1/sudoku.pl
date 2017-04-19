724cf881b2f71268e2851316361a6b6c6af77e16
Weiran Guo <guowr97@pc33.cs.ucdavis.edu>
2017-04-18 20:09:17 -0700

    revised

--- HW1/sudoku.pl
+++ HW1/sudoku.pl
@@ -204,8 +204,42 @@ printsudoku([H|T]) :-
 
 
 % Expects a list of lists 9 by 9 grid.
-sudoku(L) :-
+sudoku(L) :- %a simple version of sudoku solver
+	L = [
+	     [A1, A2, A3, A4, A5, A6, A7, A8, A9], 
+	     [B1, B2, B3, B4, B5, B6, B7, B8, B9],
+             [C1, C2, C3, C4, C5, C6, C7, C8, C9],
+             [D1, D2, D3, D4, D5, D6, D7, D8, D9],
+             [E1, E2, E3, E4, E5, E6, E7, E8, E9],
+             [F1, F2, F3, F4, F5, F6, F7, F8, F9],
+             [G1, G2, G3, G4, G5, G6, G7, G8, G9],
+             [H1, H2, H3, H4, H5, H6, H7, H8, H9],
+             [I1, I2, I3, I4, I5, I6, I7, I8, I9]],
 
 % YOU NEED TO COMPLETE THIS PREDICATE, PLUS PROVIDE ANY HELPER PREDICATES BELOW.
 
 
+validrow(Val,Row):-
+	value(Val),
+	Row = [H|T],
+	member(Val,
+	unique
+
+validcol(Val,Col):-
+
+validsquarr(Val,Square):-
+
+remain(Rows,Cols,Sqrs,Val,Rowrem,Colrem,Sqrrem):-
+	exclude(Rows,Val,Rowrem),
+	exclude(Cols,Val,Colrem),
+	exclude(Sqrs,Val,Sqrrem).
+
+exclude([H|T],H,T).
+exclude([H|T],Val,[H|R]:-
+	exclude(T,Val,R).
+
+transpose([],[]).
+transpose([F|R],Tm) :-
+
+transpose(Matrix,IMatrix):-
+	Matrix 
