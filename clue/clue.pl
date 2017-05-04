498efb301d55c624dc9589a0acdfd41bce4dfc47
Weiran Guo <guowr97@pc29.cs.ucdavis.edu>
2017-05-04 13:57:27 -0700

    some functions

--- clue/clue.pl
+++ clue/clue.pl
@@ -6,7 +6,7 @@ character(white).
 character(peacock).
 
 weapon(pipe).
-weapen(knife).
+weapon(knife).
 weapon(wrench).
 weapon(candlestick).
 weapon(rope).
@@ -23,42 +23,51 @@ room(library).
 room(study).
 
 clue :-
-	init,
+	%init,
 	menu,
 	play.
 
 menu :-
 	write('Clue Assistant V1.0\n'),
-	write('Character list:\n'),
+	write('------Character list:\n'),
 	findall(N1,character(N1),L1),
 	printline(L1),
-	write('Weapon list:'),
+	write('------Weapon list:\n'),
 	findall(N2,weapon(N2),L2),
 	printline(L2),
-	write('Room list'),
-	findall(N3,room(N2),L3),
+	write('------Room list\n'),
+	findall(N3,room(N3),L3),
 	printline(L3).
 
 printline([]).
-printline(H|T) :-
+printline([H|T]) :-
 	write(H),nl,
 	printline(T).
 
 play :-
 	getValidNumPlayer(NumP),	
 	assert(numplayer(NumP)),
-	write('What are the cards you have right now? (eg."pipe." You can enter one at a time, enter "fin." to next step)\n'),
-	read(CardName).
+	getValidCards.
+
+getValidCards :-
+	write('What are the cards you have right now? (eg."pipe." or "green." You can enter one at a time, enter "fin." if done)\n'),
+	read(Name),
+	validName(Name),
+
+validName(fin).
+validName(Name) :-
+	character(Name),
+	
+validName(Name) :-
+	room(Name).
+validName(Name) :- 
+	weapon(Name),
 
 getValidNumPlayer(NumPlayer) :-
-        write('How many players are there? (It's better to have 3-6 players)\n example input: "3."\n'),
-        read(Num);
+        write('How many players are there? (It is better to have 3-6 players)\n example input: "3."\n'),
+        read(Num),
+	((number(Num),
+        NumPlayer = Num);
         (\+ number(Num),
-        write("You should enter a number\n",
-	getValidNumPlayer(NumPlayer));
-	(number(Num),
-	NumPlayer = Num).
-
-
-
-	
+        write("You should enter a number\n"),
+	getValidNumPlayer(NumPlayer))).
