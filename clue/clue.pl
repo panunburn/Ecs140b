e04eb727feef69f2f0ede1efd2ab85e27a5a9d36
Weiran Guo <guowr97@pc10.cs.ucdavis.edu>
2017-05-04 20:46:16 -0700

    some functions

--- clue/clue.pl
+++ clue/clue.pl
@@ -23,10 +23,18 @@ room(library).
 room(study).
 
 clue :-
-	%init,
+	init,
 	menu,
 	play.
 
+init :-
+	assert(unknown(mustard)),
+	assert(unknown(scarlet)),
+	assert(unknown(plum)),
+	assert(unknown(green)),
+	assert(unknown(white)),
+	assert(unknown(peacock)),
+	assert(unknown(hall
 menu :-
 	write('Clue Assistant V1.0\n'),
 	write('------Character list:\n'),
@@ -52,8 +60,51 @@ play :-
 	turns.
 
 turns :-
-	write("Who's turn right now? e.g. 'green'\n"),
-	read(Name),
+        write("Are you the first one? (y/n)\n"),
+        getValidChar(Char),
+        ((Char = 'y',
+	suggest,
+	win);
+        (Char = 'n',
+	turn(1))).
+
+suggest.
+turn(Num) :-
+	write("Does opponent "),write(Num), write( "reach a new room? (y/n)\n"),
+	getValidChar(Char),
+	((Char = 'y',
+	write("What character did he guess? (e.g. 'green.')\n"),
+	getValidCharacter(Name1),
+	write("What room did he guess? (e.g. 'hall.'\n"),
+	getValidRoom(Name2),
+	write("What weapon did he guess? (e.g. 'knife.'\n"),
+	getValidWeapon(Name3),
+	(
+	write("Does any player now need to/has reply to his/her suggestion? (y/n)\n"),
+	getValidChar(Char2),
+	((Char2 = 'y',
+	write("Please enter his position relative to first player, you are number 0 whereever you position is, the person after you is the position of person before you plus 1')\n"),
+	getValidNum(Num2),
+	(Num2 = 0,
+	write("if you have more than 1 card match").
+
+getValidNum(Num) :-
+	read(N),
+	numplayer(Total),
+	((N < Total,
+	Num = N);
+	(N >= Total,
+	getValidNum(Num))).
+
+
+getValidChar(Char) :-
+	read(C),
+	((C = 'y',
+	Char = 'y');
+	(C = 'n',
+	Char = 'n');
+	(write("enter 'y.' or 'n.'\n"),
+	getValidChar(Char))).
 
 iD :-
 	write("Which character did you choose?\n"),
@@ -73,19 +124,57 @@ getValidCards :-
 	write("Please enter a valid card name\n"),
 	getValidCards)).
 
+getValidCharacter(Name) :-
+	read(N),
+	((character(N),
+	Name = N);
+	(\+ character(N),
+	write("invalid character!\n"),
+	getValidCharacter(Name))).
+
+getValidRoom(Name) :-
+	read(N),
+	((room(N),
+	Name = N);
+	(\+ room(Name),
+	write("invalid room!\n"),
+	getValidRoom(Name))).
+
+getValidWeapon(Name) :-
+	read(N),
+	((weapon(N),
+	Name = N);
+	(\+ weapon(N),
+	write("invalid weapon!\n"),
+	getValidWeapn(Name))).
+
 validName(fin,_).
 validName(Name,ID) :-
 	character(Name),
-	assert(known(character(Name),ID)),
+	assert(my(Name)),
+	
 	getValidCards.
-validName(Name,ID) :-
+validName(Name.ID) :-
 	room(Name),
-	assert(known(room(Name),ID)),
+	assert(my(Name)),
 	getValidCards.
 validName(Name,ID) :- 
 	weapon(Name),
-	assert(known(weapon(Name),ID)),
+	assert(my(Name)),
 	getValidCards.
+
+validName2(fin,_).
+validName2(ID) :-
+        character(Name),
+        assert(known(character(Name),ID)),
+        getValidCards.
+validName2(ID) :-
+        room(Name),
+        assert(known(room(Name),ID)),
+        getValidCards.
+validName2(ID) :-
+        weapon(Name),
+        assert(known(weapon(Name),ID)),
 
 getValidNumPlayer(NumPlayer) :-
         write('How many players are there? (It is better to have 3-6 players)\n example input: "3."\n'),
